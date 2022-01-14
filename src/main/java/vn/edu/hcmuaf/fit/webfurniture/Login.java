4b171b4afa192e7b6ed6d262f0f589df735e14e4
BuiHoangTuanAnh-19130006 <91651051+BuiHoangTuanAnh-19130006@users.noreply.github.com>
2022-01-14 19:58:36 +0700

    upbai

---
+++ src/main/java/vn/edu/hcmuaf/fit/webfurniture/Login.java
@@ -0,0 +1,29 @@
+package vn.edu.hcmuaf.fit.webfurniture;
+
+import vn.edu.hcmuaf.fit.webfurniture.services.UserServices;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "Login", value = "/Login")
+public class Login extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        if (UserServices.getInstance().checkLogin(email, password)) {
+//            response.getWriter().println("12");
+            response.sendRedirect("http://localhost:8080/WebFurniture_war_exploded/ProductDetailsList");
+        }else {
+            request.setAttribute("error", "email or password is correct");
+            response.sendRedirect("http://localhost:8080/WebFurniture_war_exploded/DangNhap/DangNhap.jsp");
+        }
+    }
+}
