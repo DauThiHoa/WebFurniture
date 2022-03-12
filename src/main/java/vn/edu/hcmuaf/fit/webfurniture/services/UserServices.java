59bdba26a22cc22d3ef5aca391fdd2180137d856
BuiHoangTuanAnh-19130006 <91651051+BuiHoangTuanAnh-19130006@users.noreply.github.com>
2022-03-12 15:07:36 +0700

    update MD5

--- src/main/java/vn/edu/hcmuaf/fit/webfurniture/services/UserServices.java
+++ src/main/java/vn/edu/hcmuaf/fit/webfurniture/services/UserServices.java
@@ -1,6 +1,8 @@
 package vn.edu.hcmuaf.fit.webfurniture.services;
 
 import vn.edu.hcmuaf.fit.webfurniture.dao.UserDao;
+import java.math.BigInteger;
+import java.security.MessageDigest;
 
 
 
@@ -20,12 +22,24 @@ public class UserServices {
     }
 
     public boolean checkLogin(String email, String password) {
-        return UserDao.getInstance().checkLogin(email, password);
+        return UserDao.getInstance().checkLogin(email, hashPasword(password));
     }
 
     public boolean register(String username, String password, String email){
-        return UserDao.getInstance().register(username, password,email);
+        return UserDao.getInstance().register(username, hashPasword(password) ,email);
 
+    }
+private String hashPasword(String pass) {
+        try {
+            MessageDigest ma = MessageDigest.getInstance("MD5");
+            ma.update(pass.getBytes());
+            byte[] byteData = ma.digest();
+            BigInteger number = new BigInteger(1, byteData);
+            return number.toString(16);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
